284f452b824e097391b946f1dde961d4a07827db
Dustin Leary <dleary@dleary.com>
2019-10-21 20:34:48 -0700

    Create finale-dev-setup.ps1

---
+++ finale-dev-setup.ps1
@@ -0,0 +1,6 @@
+# This script installs chocolatey and a few packages
+
+# To run this script, right-click the windows button, choose "Windows Powershell (Admin)", and then paste the following line:
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
